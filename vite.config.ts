b74f0f47540082da172e4f926812f5e817073edd
divoglav <divoglav@protonmail.com>
2024-10-10 00:00:21 +0300

    feat: glsl support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), glsl()],
 });
